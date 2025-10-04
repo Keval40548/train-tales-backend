f74192948723d0e305be6b31eef65121bde50a71
Keval Ambasana <kevalambasana@gmail.com>
2025-10-04 18:29:43 +0530

    Adds new API endpoints

--- schema.py
+++ schema.py
@@ -7,19 +7,37 @@ class TrainCompositionSchema(BaseModel):
     boardingStation: str
 
 class CoachCompositionSchema(BaseModel):
-    trainNo: str
     boardingStation: str
+    trainNo: str
     remoteStation: str
     trainSourceStation: str
     jDate: str
     coach: str
     cls: str
 
+    model_config = {
+        "extra": "ignore"
+    }
+
 class VacantBerthSchema(BaseModel):
-    boardingStation: str
     chartType: Optional[int] = Field(default=1)
     cls: str
     jDate: str
     remoteStation: str
     trainNo: str
     trainSourceStation: str
+    boardingStation: str
+    
+    model_config = {
+        "extra": "ignore"
+    }
+
+class BerthBetweenStationsSchema(BaseModel):
+    trainNo: str
+    jDate: str
+    boardingStation: str
+    boardingStationNumber: int
+    destinationStation: str
+    destinationStationNumber: int
+    isAC: bool = Field(default=True)
+    isNonAC: bool = Field(default=True)
