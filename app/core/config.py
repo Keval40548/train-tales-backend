66d92f7f55942b1592f473af4592303c195bef14
Keval Ambasana <kevalambasana@gmail.com>
2025-10-19 15:16:03 +0530

    Adds Redis connection and methods to set and get hash

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,8 @@
 import redis.asyncio as redis
+from fastapi import HTTPException
+from typing import Optional
 import os
 import json
-
-redis_client = redis.Redis(host="localhost", port=6379, db=0)
 
 envVariables = {
     "charting_url": os.environ["CHARTING_URL"],
@@ -17,3 +17,39 @@ origins = [
     "http://10.200.55.86:4200",
     "http://localhost:4200",
 ]
+
+redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+
+
+async def set_redis_hash(hash_name, key, value, expiry: Optional[int]):
+    try:
+        key_entry_status = await redis_client.hset(name=hash_name, key=key, value=value)
+        if key_entry_status and expiry:
+            has_expiry_updated = await redis_client.hexpire(
+                hash_name, expiry, key, nx=True
+            )
+            if has_expiry_updated == 0:
+                print(f"Something went wrong! Expiry not set for {key} in {hash_name}.")
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Unable to write the data to Redis due to Error: {e}",
+        )
+
+
+async def get_redis_hash(hash_name, key):
+    try:
+        fetched_key = await redis_client.hget(name=hash_name, key=key)
+        try:
+            response = json.loads(fetched_key)
+        except (json.JSONDecodeError, TypeError):
+            response = fetched_key
+
+        return response
+
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Unable to fetch the data from Redis due to Error: {e}",
+        )
--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
 from app.api.trains_routes import trains
 from app.api.berth_routes import berths
-from app.core.config import origins
+from app.core.config import origins, redis_client
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
-app = FastAPI(title="Train Tales")
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    try:
+        redis_startup_state = await redis_client.ping()
+        if redis_startup_state:
+            print("Redis Server is up and running!")
+        app.state.redis = redis_client
+        yield
+
+    except Exception as e:
+        print(f"Unable to connect to the Redis server due to Error(s): {e}")
+
+    finally:
+        await app.state.redis.close()
+
+
+app = FastAPI(lifespan=lifespan, title="Train Tales")
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,7 +35,6 @@ app.add_middleware(
 
 app.include_router(trains, prefix="/trains")
 app.include_router(berths, prefix="/berths")
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", port=8000, host="0.0.0.0", reload=True)
