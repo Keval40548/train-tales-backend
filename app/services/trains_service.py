be789a5d62b945abc9af2cfa2498d7995b1bcfe9
Keval Ambasana <kevalambasana@gmail.com>
2025-10-19 15:17:20 +0530

    Fetches train schedule from Redis, if exists

--- app/services/trains_service.py
+++ app/services/trains_service.py
@@ -1,6 +1,8 @@
 from fastapi import HTTPException
+from app.core.config import get_redis_hash, set_redis_hash
 import requests
 import logging
+import json
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -28,13 +30,26 @@ def get_all_trains_service(url, headers):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-def get_train_schedule_service(url, headers):
-    try:
-        response = requests.request("GET", url, headers=headers, timeout=60)
-        response.raise_for_status()
-        return response.json()
+async def get_train_schedule_service(url, headers, train_number):
+    response = await get_redis_hash("train_schedules", train_number)
 
-    except requests.exceptions.HTTPError as e:
-        raise HTTPException(status_code=e.response.status_code, detail=str(e))
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    if response is None:
+        try:
+            response = requests.request("GET", url, headers=headers, timeout=60)
+            response.raise_for_status()
+            response = response.json()
+            try:
+                await set_redis_hash(
+                    "train_schedules", train_number, json.dumps(response), 43200
+                )
+            except Exception as e:
+                print(
+                    f"Unable to set schedule of {train_number} to Redis due to error: {e}"
+                )
+
+        except requests.exceptions.HTTPError as e:
+            raise HTTPException(status_code=e.response.status_code, detail=str(e))
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+    return response
